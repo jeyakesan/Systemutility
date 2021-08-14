1931a4d8abe8269ac02109e5d432e19a7fca930e
jeyakesan <jeyakesan@gmail.com>
2021-08-15 01:08:22 +0530

    Added the core and thread Info

--- Processorinfo.py
+++ Processorinfo.py
@@ -1,5 +1,9 @@
+import psutil
 import platform
 import cpuinfo
 uname = platform.uname()
 print (f"Processor brand  : {cpuinfo.get_cpu_info()['brand_raw']}")
 print(f"Processor: {uname.processor}")
+print("No.of cores:", psutil.cpu_count(logical=False))
+print("No.of Threads (i.e order of instructions that can be passed in a single core):", psutil.cpu_count(logical=True))
+
